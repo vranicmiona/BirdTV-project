f45064b915897e50c36ca6dcde737b9532b4ffa9
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-04-26 23:41:13 +0200

    BirdTV-new commit

--- Project-BirdTV/app.js
+++ Project-BirdTV/app.js
@@ -7,5 +7,35 @@ const getVideos = async () => {
 
 const data = await response.json();
 data.Video.forEach((video) => {
-  const videoItems = document.createElement("div");
+  const videoItem = document.createElement("div");
+  videoItem.className = "video-item";
+  const slikaVidea = document.createElement("img");
+  slikaVidea.src = video.snapshots.sd;
+
+  const naslov = document.createElement(`h1`);
+  naslov.textContent = video.name;
+
+  const trajanje = document.createElement("span");
+
+  const link = document.createElement("a");
+  link.className = "linkovi";
+  link.href = video.source.hd;
+
+  link.appendChild(slikaVidea);
+  link.appendChild(naslov);
+  link.appendChild(trajanje);
+  link.appendChild(videoItem);
+
+  function trajanjeVidea(vreme) {
+    const sati = Math.floor(vreme / 3600);
+    const minuti = Math.floor(vreme % 3600) / 60;
+    const sekunde = Math.floor(vreme % 60);
+
+    return `${sati.toString().padStart(2, "0")}:${minuti
+      .toString()
+      .padStart(2, "0")} : ${sekunde.toString().padStart(2, "0")}`;
+  }
+  trajanje.textContent = trajanjeVidea(+video.duration);
 });
+
+getVideos();
